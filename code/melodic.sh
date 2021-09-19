592e2fe25c508ea02d662f6beee3471d32f12edf
DVSneuro <david.v.smith@temple.edu>
2021-09-18 23:36:47 -0400

    initial commit

---
+++ code/melodic.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This script will run melodic on all of the good runs from the trus task.
+# It will also calculate the similarity (correlation) with the Smith09 maps.
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+task=trust
+
+# run melodic: auto-dim
+melodic -i ${scriptdir}/melodic_filelist.txt \
+-o melodic-concat_dim-00_task-${task}.ica -v \
+--nobet --bgimage=mean_func \
+--report --guireport=report.html -d 0 \
+--mmthresh=0.5 --Ostats -a concat
+
+# run melodic: 25 components
+melodic -i ${scriptdir}/melodic_filelist.txt \
+-o melodic-concat_dim-25_task-${task}.ica -v \
+--nobet --bgimage=mean_func \
+--report --guireport=report.html -d 25 \
+--mmthresh=0.5 --Ostats -a concat
