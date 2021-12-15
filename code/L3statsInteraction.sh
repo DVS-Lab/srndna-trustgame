7b36c118bbc135d194cef35916a72d20b9f92a01
DVSneuro <david.v.smith@temple.edu>
2021-12-15 12:12:40 -0500

    initial commit

---
+++ code/L3statsInteraction.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# This script will perform Level 3 statistics in FSL.
+# Rather than having multiple scripts, we are merging three analyses
+# into this one script:
+#		1) two groups (older vs. younger)
+#		2) two groups (older vs. younger), with covariates
+#		3) single group average
+#
+# This script can also run randomise (permutation-based stats) on existing output.
+# By default, randomise will not be be run if FEAT analyses do not exist. In addition,
+# randomise will only be run on copes above a specified number (see copenum_thresh_randomise variable).
+# If you have no intention of running randomise, you set copenum_thresh_randomise=20 (> max of 19 copes)
+# and you could uncomment out the rm lines that remove the filtered_func_data file (save disk space).
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+# study-specific inputs and general output folder
+task=trust
+N=48
+copenum=$1
+copenum_thresh_randomise=10 # actual contrasts start with cope10 (rec > def). no need to do randomise main effects (e.g., rec > nothing/fixation/baseline)
+copename=$2
+REPLACEME=$3 # this defines the parts of the path that differ across analyses
+MAINOUTPUT=${maindir}/derivatives/fsl/L3_model-01_task-${task}_n${N}_flame1+2_interaction
+mkdir -p $MAINOUTPUT
+
+
+
+# set outputs and check for existing
+cnum_pad=`zeropad ${copenum} 2`
+OUTPUT=${MAINOUTPUT}/L3_task-${task}_${REPLACEME}_cnum-${cnum_pad}_cname-${copename}
+if [ -e ${OUTPUT}.gfeat/cope1.feat/cluster_mask_zstat1.nii.gz ]; then
+
+	# run randomise if output doesn't exist and the contrasts (copes) are valid
+	cd ${OUTPUT}.gfeat/cope1.feat
+	if [ ! -e randomise_tfce_corrp_tstat4.nii.gz ] && [ $copenum -ge $copenum_thresh_randomise ]; then
+		randomise -i filtered_func_data.nii.gz -o randomise -d design.mat -t design.con -m mask.nii.gz -T -c 2.6 -n 10000
+	fi
+
+else # try to run feat and clean up previous effort with partial output
+
+	echo "re-doing: ${OUTPUT}" >> re-runL3.log
+	rm -rf ${OUTPUT}.gfeat
+
+	# create template and run FEAT analyses
+	ITEMPLATE=${maindir}/templates/L3_template_n${N}_${task}_interactionCovs.fsf
+	OTEMPLATE=${MAINOUTPUT}/L3_task-${task}_${REPLACEME}_copenum-${copenum}_interactionCovs.fsf
+	sed -e 's@OUTPUT@'$OUTPUT'@g' \
+	-e 's@COPENUM@'$copenum'@g' \
+	-e 's@REPLACEME@'$REPLACEME'@g' \
+	-e 's@BASEDIR@'$maindir'@g' \
+	<$ITEMPLATE> $OTEMPLATE
+	feat $OTEMPLATE
+
+	# delete unused files
+	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/res4d.nii.gz
+	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/corrections.nii.gz
+	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/stats/threshac1.nii.gz
+	#rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/filtered_func_data.nii.gz
+	rm -rf ${OUTPUT}.gfeat/cope${cope}.feat/var_filtered_func_data.nii.gz
+
+fi
---
+++ code/run_L3statsInteraction.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This run_* script is a wrapper for L3stats.sh, so it will loop over several
+# copes and models. Note that Contrast N for PPI is always PHYS in these models.
+
+
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+
+# this loop defines the different types of analyses that will go into the group comparisons
+for analysis in act nppi-dmn nppi-ecn ppi_seed-PCC ppi_seed-rFFA ppi_seed-rTPJ ppi_seed-VMPFCface ppi_seed-VMPFCwin ppi_seed-VS; do
+	analysistype=type-${analysis}
+
+	# these define the cope number (copenum) and cope name (copename)
+	for copeinfo in "1 c_C" "2 c_F" "3 c_S" "4 C_def" "5 C_rec" "6 F_def" "7 F_rec" "8 S_def" "9 S_rec" "10 rec-def" "11 face" "12 rec-def_F-S" "13 F-S" "14 F-C" "15 S-C" "16 rec_SocClose" "17 def_SocClose" "18 rec-def_SocClose" "19 phys"; do
+
+		# split copeinfo variable
+		set -- $copeinfo
+		copenum=$1
+		copename=$2
+
+		# skip non-existent contrast for activation analysis
+		if [ "${analysistype}" == "type-act" ] && [ "${copeinfo}" == "19 phys" ]; then
+			echo "skipping phys for activation since it does not exist..."
+			continue
+		fi
+
+		NCORES=30
+		SCRIPTNAME=${maindir}/code/L3statsInteraction.sh
+		while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+			sleep 1s
+		done
+		bash $SCRIPTNAME $copenum $copename $analysistype &
+
+	done
+done
